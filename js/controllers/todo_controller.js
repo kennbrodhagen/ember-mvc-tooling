52491390b3a8fd4c60b82a9123c1476bcb534eb3
Kenn Brodhagen <kennbrodhagen@gmail.com>
2013-12-07 07:44:49 -0500

    Ember todo complete from tutorial.

---
+++ js/controllers/todo_controller.js
@@ -0,0 +1,39 @@
+Todos.TodoController = Ember.ObjectController.extend({
+    actions: {
+        acceptChanges: function() {
+            this.set('isEditing', false);
+
+            if (Ember.isEmpty(this.get('model.title'))) {
+                this.send('removeTodo');
+            } else {
+                this.get('model').save();
+            }
+        },
+
+        editTodo: function() {
+            this.set('isEditing', true);
+        },
+
+        removeTodo: function() {
+            var todo = this.get('model');
+            todo.deleteRecord();
+            todo.save();
+        }
+    },
+
+    isCompleted: function(key, value) {
+        var model = this.get('model');
+
+        if (value == undefined) {
+            // property being used as getter
+            return model.get('isCompleted');
+        } else {
+            // property being used as setter
+            model.set('isCompleted', value);
+            model.save();
+            return value;
+        }
+    }.property('model.isCompleted'),
+
+    isEditing: false
+});
